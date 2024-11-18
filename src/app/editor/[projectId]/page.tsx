50b7150c2545381041a88b115c9a0198eceb278d
Boris Stanar <bstanar96@gmail.com>
2024-11-18 16:42:46 +0100

    Initial project setup. Editor canvas setup

---
+++ src/app/editor/[projectId]/page.tsx
@@ -0,0 +1,11 @@
+import { EditorComponent } from "@/features/editor/components/editor";
+
+const EditorProjectIdPage = () => {
+   return(
+      <div className="h-full"> 
+         <EditorComponent/>
+      </div>
+   );
+};
+
+export default EditorProjectIdPage;
